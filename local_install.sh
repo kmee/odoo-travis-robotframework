2a575d186615e9d9de2ecea9d3ee02e406b82aca
Luis Felipe Miléo <mileo@kmee.com.br>
2016-10-28 09:07:15 -0200

    Update local_install.sh

--- local_install.sh
+++ local_install.sh
@@ -4,8 +4,8 @@ bin/pip install --upgrade pip setuptools zc.buildout
 echo "[buildout]
 extends = test.cfg
 
-#[openerp]
-#options.db_user = odoo
+[openerp]
+options.admin_passwd = admin
 #options.db_password = odoo
 #options.db_host = localhost
 #options.db_port = 5432" >> buildout.cfg
