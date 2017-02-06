4348173d1d6834631add82647c030be9646d2cbe
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2017-02-06 19:19:27 -0200

    [REF] local install script

--- local_install.sh
+++ local_install.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 virtualenv . --system-site-packages
 bin/pip install --upgrade pip setuptools zc.buildout
+
+sudo apt-get install expect-dev python-lxml libappindicator1 fonts-liberation chromium-browser chromium-chromedriver
+
+wget http://chromedriver.storage.googleapis.com/2.21/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo chmod u+x chromedriver
+sudo mv chromedriver /usr/bin/
+export CHROME_BIN=chromium-browser
+
+
 echo "[buildout]
 extends = test.cfg
 
