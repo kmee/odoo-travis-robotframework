13c2eca4b57ca44888f154d90e5ce8e69dd6d0ee
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2017-04-04 18:41:57 -0300

    [FIX] Instala dependências do Firefox

--- local_install.sh
+++ local_install.sh
@@ -6,10 +6,18 @@ sudo apt-get install expect-dev python-lxml libappindicator1 fonts-liberation ch
 
 wget http://chromedriver.storage.googleapis.com/2.21/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
+rm chromedriver_linux64.zip
+rm chromedriver_linux64.zip.1
+rm chromedriver_linux64.zip.2
 sudo chmod u+x chromedriver
 sudo mv chromedriver /usr/bin/
 export CHROME_BIN=chromium-browser
 
+wget https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz
+tar xvfz geckodriver-v0.15.0-linux64.tar.gz
+rm geckodriver-v0.15.0-linux64.tar.gz
+sudo chmod u+x geckodriver
+sudo mv geckodriver /usr/bin/
 
 echo "[buildout]
 extends = test.cfg
